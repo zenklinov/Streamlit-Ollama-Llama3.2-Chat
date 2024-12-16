d1d15c6a6c87abbba53ae1220a2395ffdc372df4
Amanatullah Pandu Zenklinov <91674858+zenklinov@users.noreply.github.com>
2024-12-16 11:21:11 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from langchain_community.llms import Ollama
+import streamlit as st
+import torch
+
+# Check if CUDA is available
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+llm = Ollama (model = "llama3.2")
+
+st.title("Chatbot using llama3.2")
+
+prompt = st.text_area("Enter your prompt:")
+
+if st.button("Generate"):
+    if prompt:
+        with st.spinner ("Generating response..."):
+            st.write(llm.invoke(prompt,stop=['<|eot_id|>']))
